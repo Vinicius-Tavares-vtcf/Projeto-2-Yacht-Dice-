ae7e81216485a9b5e2f1589a2ced38b6fe3d0e6d
judahlevy156-alt <judahlevy156@gmail.com>
2026-04-21 15:31:13 -0300

    Question 2: Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,16 @@ def rolar_dados (dados):
         sorteio.append(sorteado)
     return sorteio
 
+def guardar_dado(dados_rolados, dados_guardados, indice_dado):
+    novos_rolados = []
+    novos_guardados = []
+    for x in dados_guardados:
+        novos_guardados.append(x)
+    
+    novos_guardados.append(dados_rolados[indice_dado])
+    
+    for i in range(len(dados_rolados)):
+        if i != indice_dado:
+            novos_rolados.append(dados_rolados[i])
+    
+    return [novos_rolados, novos_guardados]
